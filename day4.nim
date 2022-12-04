ce1e15b985a0cba647aad661df3319203cc4e9eb
slava0135 <super.novalskiy_0135@inbox.ru>
2022-12-04 11:10:32 +0300

    day 4

---
+++ day4.nim
@@ -0,0 +1,26 @@
+import std/strutils
+
+func solve(lines: openArray[string]): (int, int) =
+  var full_overlaps = 0
+  var overlaps = 0
+  for line in lines:
+    if line.len == 0:
+      break
+    let ranges = line.split(',')
+    let first = ranges[0].split('-')
+    let second = ranges[1].split('-')
+    var first_sections: set[uint16]
+    var second_sections: set[uint16]
+    for i in first[0].parseUInt()..first[1].parseUInt():
+      first_sections.incl i.uint16
+    for i in second[0].parseUInt()..second[1].parseUInt():
+      second_sections.incl i.uint16
+    if first_sections <= second_sections or first_sections >= second_sections:
+      full_overlaps += 1
+    if (first_sections * second_sections).len != 0:
+      overlaps += 1
+  return (full_overlaps, overlaps)
+
+echo solve(readFile("input/4_test.txt").splitLines)
+echo solve(readFile("input/4.txt").splitLines)
+
